445d1c2433c9ec8627fb76f60ad44e1acd2679de
Timothy McLane <timothyamclane@gmail.com>
2020-04-21 08:29:22 -0500

    Adding enchanting to supported professions
    Enchanting requires a custom version of LWC until it supports multicrafting for enchanting

--- Queue.lua
+++ Queue.lua
@@ -191,18 +191,31 @@ local CRAFTING_BONUSES = {
   [CRAFTING_TYPE_CLOTHIER] = NON_COMBAT_BONUS_CLOTHIER_LEVEL,
   [CRAFTING_TYPE_WOODWORKING] = NON_COMBAT_BONUS_WOODWORKING_LEVEL,
   [CRAFTING_TYPE_JEWELRYCRAFTING] = NON_COMBAT_BONUS_JEWELRYCRAFTING_LEVEL,
+  [CRAFTING_TYPE_ENCHANTING] = NON_COMBAT_BONUS_ENCHANTING_LEVEL
 }
+
+local TA = 45850
+local ESSENCE_IDS ={
+  45833,
+  45831,
+  45832
+}
+local POTENCY_IDS = {
+  [10] = 64509,
+}
+
 -- DolgubonGlobalDebugOutput = function(...) d(...) end
 function LazierCrafterQueue:New()
   local obj = ZO_Object.New(self)
   self:Initialize()
+  self.isDebug = false
   return obj
 end
 
 local logDisplayNames = {['@Aldanga'] = true, ['@FoodandStuff'] = true}
 
-function LazierCrafterQueue.Log(...)
-  if logDisplayNames[GetDisplayName()] then
+function LazierCrafterQueue:Log(...)
+  if self.isDebug and logDisplayNames[GetDisplayName()] then
     d(...)
   end
 end
@@ -210,14 +223,14 @@ end
 -- Events are in LibLazyCrafting.lua:594
 function LazierCrafterQueue:CallbackFunction()
   return function (event, craftingType, requestTable)
-    self.Log(event, craftingType, requestTable)
+    self:Log(event, craftingType, requestTable)
     -- We're done at the station and haven't left yet, so let's leave
     if event == LLC_NO_FURTHER_CRAFT_POSSIBLE and GetCraftingInteractionType() ~= 0 and self.QueueActive[craftingType] then
-      self.Log('We should exit now')
+      self:Log('We should exit now', requestTable)
       self.QueueActive[craftingType] = false
       SCENE_MANAGER:HideCurrentScene()
     elseif event == LLC_INSUFFICIENT_MATERIALS then
-      self.Log('Not enough mats!')
+      self:Log('Not enough mats!')
     end
   end
 end
@@ -246,18 +259,26 @@ end
 --  overrideNonMulticraft)
 function LazierCrafterQueue:AddProfession(professionId, multiplier)
   self.QueueActive[professionId] = true
-  local craftingPassive = GetNonCombatBonus(CRAFTING_BONUSES[professionId])
-  local isCP, craftingLevel = unpack(WRIT_LEVELS[professionId][craftingPassive])
-  local craftableItems = ZO_DeepTableCopy(DAILY_ROTATION[professionId])
-  for k, item in ipairs(craftableItems) do
-    item[2] = isCP
-    item[3] = craftingLevel
-    item[7] = professionId
-    item[15] = multiplier * item[15]
-    ZO_DeepTableCopy(defaultWritOptions, item)
-  end
-  for key, craftRequest in pairs(craftableItems) do
-    self.InteractionTable:CraftSmithingItemByLevel(unpack(craftRequest))    
+  if professionId == CRAFTING_TYPE_ENCHANTING then
+    local craftingPassive = GetNonCombatBonus(CRAFTING_BONUSES[professionId])
+    local potency = POTENCY_IDS[craftingPassive]
+    for k, essence in pairs(ESSENCE_IDS) do
+      local requestTable = self.InteractionTable:CraftEnchantingItemId(potency, essence, TA, true, nil, nil, multiplier, true)
+    end
+  else
+    local craftingPassive = GetNonCombatBonus(CRAFTING_BONUSES[professionId])
+    local isCP, craftingLevel = unpack(WRIT_LEVELS[professionId][craftingPassive])
+    local craftableItems = ZO_DeepTableCopy(DAILY_ROTATION[professionId])
+    for k, item in ipairs(craftableItems) do
+      item[2] = isCP
+      item[3] = craftingLevel
+      item[7] = professionId
+      item[15] = multiplier * item[15]
+      ZO_DeepTableCopy(defaultWritOptions, item)
+    end
+    for key, craftRequest in pairs(craftableItems) do
+      self.InteractionTable:CraftSmithingItemByLevel(unpack(craftRequest))    
+    end
   end
 end
 
